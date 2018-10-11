dea5ee1ea8af5c1f1a4691afb6b43729ae4ceaaa
p16204023 <p16204023@GWHXX631WIN005W>
2018-10-11 13:36:10 +0100

    yeah, this instructions are terrible

---
+++ lab 1/Source.cpp
@@ -0,0 +1,35 @@
+
+#include "stdafx.h"
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+#include <iostream>
+
+
+int main()
+{
+	srand(time(NULL));
+	int num = rand() % 101;
+	int guesses = 5;
+	while (guesses > 0) {
+		cout << "type a guess" << endl;
+		int guess;
+		cin >> guess;
+		if (guess > num) {
+			cout << "too high, " << guesses << " tries remaining" << endl;
+		}
+		else if (guess < num) {
+			cout << "too low, " << guesses << " tries remaining" << endl;
+		}
+		else if (guess == num) {
+			cout << "correct, with " << guesses << " to spare";
+		}
+		else {
+			cout << "entirely incorrect, have an extra try" << endl;
+			guesses++;
+		}
+		guesses--;
+	}
+	return 0;
+}
+
